94d59b2fa5d301295fb56b50dda3a92598b9799f
SanjanaReddy2005 <saisanjanareddyalgubelly@gmail.com>
2024-02-05 15:34:02 +0530

    Lab4

--- Lab4/inputfiles/1.cpp
+++ Lab4/inputfiles/1.cpp
@@ -6,9 +6,13 @@ int main(){
     ofstream input;
     input.open("../1.txt",ios::in);
     input << 10 << endl;
-    input << 10000 << endl;
-    for(int i=0;i<10000;i++){
-        input << rand() << " ";
+    int t = 10;
+    while(t--){
+        input << 10000 << endl;
+        for(int i=0;i<10000;i++){
+            input << rand() << " ";
+        }
+        input << endl;
     }
-    input << endl;
+    
 }
